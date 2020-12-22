f016ade64abc8279f77e2143c63f730eb48f0988
Daniel Covill <daniel.covill@gmail.com>
2020-12-21 20:52:56 -0700

    Initial Checkin

--- LinkedList.js
+++ LinkedList.js
@@ -1,43 +1,43 @@
-import Node from './Node';
+const Node = require('./Node.js');
 class LinkedList { 
-    #head;
-    #size;
 
     constructor() 
     { 
-        this.head = null; 
-        this.size = 0; 
     } 
   
-    // Adds an element to the head of the linked list
+    // Adds an element to the head of the linked list and returns nothing
     push(element) {
-
     }
 
     // Removes and returns the element at the head of the linked list
     pop() {
-
     }
 
     // Returns the element at the head of the linked list without altering the list
     peek() {
-
     }
 
     // Adds an element to the point in the linked list defined by the zero 
-    // indexed location parameter
+    // indexed location parameter and returns nothing
     insertAt(element, location) {
-
     }
 
     // Removes the element at the point in the linked list defined by the zero 
     // indexed location parameter
     removeFrom(location) {
-
     }
 
-    // Removes the first element that matches the element parameter
+    // Removes and returns the first element that matches the element parameter
+    // If no element is found, throws exception
     removeElement(element) {
-
     }
+
+    // Returns a comma and space separated list of values representing the contents of the
+    // LinkedList. E.g. if I add the integers 1 through 4, it would return the following:
+    // 1, 2, 3, 4
+    // Empty LinkedLists return an empty string
+    toString() {
+    }
+
 } 
+module.exports = LinkedList;
---
+++ setup.md
@@ -0,0 +1,10 @@
+In order to use private Javascript variables, you need to be using Node 12. You can see the 
+version of Node you're using by typing 'node -v' at the command prompt. 
+
+The following commands upgrade your linux instance and add a new source for getting node updates
+- sudo apt update && sudo apt upgrade
+- sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
+- curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+
+Once that's complete, you can run the following to upgrade Node to 12
+- sudo apt-get install -y nodejs
